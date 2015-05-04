6f00d686ea6bc7044d22e40e649bf5a8aa3701f0
litex2x <ritchiemarihugh@gmail.com>
2015-05-04 11:21:27 -0700

    Included missing project files and updated ignore

---
+++ WebApplication/Models/PunchResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodePound.PunchOutCalculator.WebApplication.Models
+{
+    public class PunchResponse
+    {
+        public string Result { get; set; }
+        public bool IsSuccessful { get; set; }
+    }
+}
---
+++ WebApplication/Scripts/punch-cruncher.js
@@ -0,0 +1,26 @@
+﻿angular.module('punchCruncher', ['ui.bootstrap'])
+    .controller('punchCruncherController', function ($scope, $http) {
+        $scope.request = {
+            'PunchIn': new Date(),
+            'LunchOut': new Date(),
+            'LunchIn': new Date(),
+            'TargetTotalMinutes': 480
+        };
+
+        $scope.request.PunchIn.setHours(8, 0, 0);
+        $scope.request.LunchOut.setHours(12, 0, 0);
+        $scope.request.LunchIn.setHours(12, 30, 0);
+
+        $scope.calculate = function (request) {
+            $scope.success = $scope.error = null;
+
+            $http.post('/api/calculation', request).success(function (data) {
+                if (data.IsSuccessful) {
+                    var resultDate = new Date(data.Result);
+                    $scope.success = resultDate.toLocaleTimeString();
+                } else {
+                    $scope.error = data.Result;
+                }
+            });
+        };
+    });
